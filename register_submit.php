75075e670171cd930a07ece30e9f793b3fa0e236
nabin <nabdabop10@gmail.com>
2024-07-22 10:04:06 +0545

    ok

--- register_submit.php
+++ register_submit.php
@@ -7,8 +7,10 @@ $email=get_safe_value($con,$_POST['email']);
 // $mobile=get_safe_value($con,$_POST['mobile']);
 $password=get_safe_value($con,$_POST['password']);
 
+
 $check_user=mysqli_num_rows(mysqli_query($con,"select * from users where email='$email'"));
 // $check_mobile=mysqli_num_rows(mysqli_query($con,"select * from users where mobile='$mobile'"));
+
 if($check_user>0){
 	echo "email_present";
 // }elseif($check_mobile>0){
